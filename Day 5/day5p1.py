adf7c04ba2b31855e4f7bfdd987580b261e68ae5
DanielDowling999 <ddow731@aucklanduni.ac.nz>
2024-12-14 14:45:08 +1300

    Completed day 5 p1

--- Day 5/day5p1.py
+++ Day 5/day5p1.py
@@ -1,4 +1,4 @@
-with open('day 5/input.txt', 'r') as update:
+"""with open('day 5/input.txt', 'r') as update:
     ruleDict = {}
     line = update.readline()
     middle = 0
@@ -11,12 +11,61 @@ with open('day 5/input.txt', 'r') as update:
             ruleDict[r1] = [r2]
         line = next(update)
         #print(line)
-    #print(ruleDict['23'])
+    print("Rules: ",  ruleDict['22'])
     line = next(update)
     while line !='\n':
-        line = line.strip().split(',')
+        curr = line.strip().split(',')
+        seenSelf = False
+        print(curr)
         try:
+            for rule in curr:
+                if rule in ruleDict[curr]:
+                    if seenSelf == True:
+                        
+                    else:
+                        break;
             line = next(update) 
         except:
             break
-    print(middle)
+    print(middle)"""
+def compareFn(a,b, rules):
+    #print(a,b)
+    if [a,b] in rules:
+        return 1
+    elif [b,a] in rules:
+        return 0
+    else:
+        return 1
+with open('day 5/input.txt', 'r') as update:
+    rules = []
+    updates = []
+    line = update.readline()
+    while line != '\n':
+        rules.append((line.strip().split('|')))
+        line = next(update)
+    #print(rules)
+    line = next(update)
+    while line != '\n':
+        try:
+            updates.append(line.strip().split(','))
+            line = next(update)
+        except:
+            break
+    mid = 0
+    for update in updates:
+        for i in range(len(update)-1):
+            correct = True
+            for j in range(i+1, len(update)):
+                if compareFn(update[i],update[j],rules) == 1:
+                    continue
+                else:
+                    #print("line: ", update, " broken")
+                    correct = False
+                    break
+            if correct == False:
+                break
+        if correct:
+            addMid = int(update[int(len(update)/2)])
+            mid += addMid
+    print(mid)    
+    #print(updates)
